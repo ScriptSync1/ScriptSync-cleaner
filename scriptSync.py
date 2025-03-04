44364c2fbb6fd413152c1853539567fed72f251f
ScriptSync1 <kriskrainis@gmail.com>
2025-03-03 19:08:19 -0500

    Add files via upload

---
+++ scriptSync.py
@@ -0,0 +1,30 @@
+import shutil, os, webbrowser
+
+def open_browser():
+    webbrowser.open("https://scriptsync.tilda.ws/")
+open_browser()
+folder = 'c:\windows\Temp'
+folder3 = 'c:\windows\Prefetch'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+        folder2 = 'C:$Recycle.Bin'
+for filename in os.listdir(folder2):
+    file_path = os.path.join(folder2, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+        
